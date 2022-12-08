41641a256ae666e5a25eca159bf29a9c33316bb4
Samuel Lambert <fanmc34@gmail.com>
2022-12-08 09:38:02 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BDSPScriptCommandsDecomp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
